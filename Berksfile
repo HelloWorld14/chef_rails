693913fd825fab157909a2d5c40fd5ae1cf5c224
Nikita Plusnin <nplusnin14@gmail.com>
2016-02-17 00:08:14 +0300

    add nginx, pg, rbenv and users

--- Berksfile
+++ Berksfile
@@ -1,1 +1,6 @@
 source "https://api.berkshelf.com"
+
+cookbook 'chef-solo-search'
+cookbook 'postgresql'
+cookbook 'users'
+cookbook 'rbenv'
--- site-cookbooks/nginx-server/recipes/default.rb
+++ site-cookbooks/nginx-server/recipes/default.rb
@@ -1,3 +1,12 @@
+bash 'update and upgrade server' do
+  user 'root'
+  code <<-EOC
+sudo apt-get update -y
+sudo apt-get upgrade -y
+  EOC
+end
+
+
 bash 'install nginx-server' do
   user 'root'
   code <<-EOC
